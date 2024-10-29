0b792f50072eddfee59b30704e3e55ae693bd65f
aibek-code <aibekabasov70@gmail.com>
2024-10-29 13:52:48 -0500

    Update TeamActivity.py

--- TeamActivity.py
+++ TeamActivity.py
@@ -1,6 +1,7 @@
 #Sebastian Cervantes 
 #Aibek Abasov
 #October 29, 2024 
+#This program will take a number (input) from the user and guess a randomly generated number.
 number = float(input("Please guess number 1-10; "))
 answer = int(6)
 if number == answer:
