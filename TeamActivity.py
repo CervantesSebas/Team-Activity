96f8e7a1d18a545d28d6bf31c09eaf3303223339
Sebastian Cervantes <scervantes3@my.nl.edu>
2024-10-29 13:49:24 -0500

    Update TeamActivity.py

--- TeamActivity.py
+++ TeamActivity.py
@@ -1,3 +1,12 @@
 #Sebastian Cervantes 
 #Aibek Abasov
 #October 29, 2024 
+number = float(input("Please guess number 1-10; "))
+answer = int(6)
+if number == answer:
+    print ("Well done you guessed it right ")
+elif number > answer :
+    print ("Please guess lower")
+elif number < answer:
+    print ("Please guess higher")
+  
